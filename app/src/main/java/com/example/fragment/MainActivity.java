6f7dabdd3a1403295a143578fdae81e33f6e820f
gilang <gilangyulianta07@gmail.com>
2019-03-20 23:15:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.fragment;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private SectionStatePagerAdapter mSectionsStatePagerAdapter;
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSectionsStatePagerAdapter = new SectionStatePagerAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.containter);
+        //setup the pager
+        setupViewPager(mViewPager);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionStatePagerAdapter adapter = new SectionStatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Fragment1(), "Fragment1");
+        adapter.addFragment(new Fragment2(), "Fragment2");
+        adapter.addFragment(new Fragment3(), "Fragment3");
+        viewPager.setAdapter(adapter);
+    }
+
+    public void setViewPager(int fragmentNumber) {
+        mViewPager.setCurrentItem(fragmentNumber);
+    }
+}
